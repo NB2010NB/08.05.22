fc2157585f6a1164d85b17b9a2057f98f2b9eb33
NB2010NB <natanbalai1@gmail.com>
2022-05-15 16:55:10 +0300

    js

---
+++ main.js
@@ -0,0 +1,80 @@
+// var nameCity = "rehovot"
+// console.log("rehovot")
+// document.write("natan belay")
+// alert("ברוכים הבאים")
+
+// var conTent = confirm("Are you a registered user?")
+// console.log("conTent")
+
+// var userAnswer = confirm("are you over 18 yers old?");
+// if (userAnswer) {
+//     document.write("hello");
+// }
+// else {
+//     document.write("good bye")
+// }
+// var firstName = prompt("what your first name?")
+// document.write(firstName)
+// var userAge = +prompt("What is your age?")
+// document.write(userAge)
+
+// var cantry = "ISRAEL"
+// console.log("cantry")
+
+// var FirstName = "natanBelay"
+// console.log = ("FirstName")
+
+// var number = "10"
+// console.log = ("number")
+
+var Num = 0;
+while (Num < 5) {
+    console.log("20")
+    Num++
+}
+
+var NUM1 = 2
+var NUM2 = 2
+var NUM3 = 2
+var NUM4 = 2
+var avg = ((NUM1 + NUM2 + NUM3 + NUM4) / 4)
+var sum = (NUM1 + NUM2 + NUM3 + NUM4)
+console.log("sum", "avg")
+
+
+        // var width = 150;
+        // var Length = 100;
+        // var area = (width * Length)
+        // console.log("area")
+
+
+        // var num1 = 10;
+        // var num2 = 20;
+        // var num3 = 0;
+        // console.log(num1 + "and" + num2)
+        // num3 = num1;
+        // num1 = num2;
+        // num2 = num3;
+        // console.log(num1 + "and" + num2)
+
+        // var user = prompt("Are you a registered user?");
+        // var age = prompt("Are you 18 years old?");
+        // var hello = prompt("hello");
+
+        // var num1 = +prompt("num1");
+        // var num2 = +prompt("num2");
+        // console.log( num1,num2,)
+
+
+        //  var numb1 = +prompt("numb1");
+        //  var numb2 = +prompt("numb2");
+        //  console.log( numb1,numb2,)
+
+        // var cus = +prompt("numberr");
+        // var cus1 = +prompt("nvsc");
+        // var ttt =  prompt("dipa")
+        // console.log(cus, cus1 ,ttt)
+
+        // var a1 = "a";
+        // var a2 = "1";
+        // console.log(a1, a2)
