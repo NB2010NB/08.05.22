27cfd4bb0ea282042eb2f8a026b16d849465369e
NB2010NB <natanbalai1@gmail.com>
2022-05-15 19:27:18 +0300

    many

--- main.js
+++ main.js
@@ -27,54 +27,106 @@
 // var number = "10"
 // console.log = ("number")
 
-var Num = 0;
-while (Num < 5) {
-    console.log("20")
-    Num++
+// var Num = 0;
+// while (Num < 5) {
+//     console.log("20")
+//     Num++
+// }
+
+// var NUM1 = 2
+// var NUM2 = 2
+// var NUM3 = 2
+// var NUM4 = 2
+// var avg = ((NUM1 + NUM2 + NUM3 + NUM4) / 4)
+// var sum = (NUM1 + NUM2 + NUM3 + NUM4)
+// console.log("sum", "avg")
+
+
+// var width = 150;
+// var Length = 100;
+// var area = (width * Length)
+// console.log("area")
+
+
+// var num1 = 10;
+// var num2 = 20;
+// var num3 = 0;
+// console.log(num1 + "and" + num2)
+// num3 = num1;
+// num1 = num2;
+// num2 = num3;
+// console.log(num1 + "and" + num2)
+
+// var user = prompt("Are you a registered user?");
+// var age = prompt("Are you 18 years old?");
+// var hello = prompt("hello");
+
+// var num1 = +prompt("num1");
+// var num2 = +prompt("num2");
+// console.log( num1,num2,)
+
+
+//  var numb1 = +prompt("numb1");
+//  var numb2 = +prompt("numb2");
+//  console.log( numb1,numb2,)
+
+// var cus = +prompt("numberr");
+// var cus1 = +prompt("nvsc");
+// var ttt =  prompt("dipa")
+// console.log(cus, cus1 ,ttt)
+
+// var a1 = "a";
+// var a2 = "1";
+// console.log(a1, a2)
+
+// var num = +prompt("number");
+// if (num > 10){
+//     document.write("greater then 10")
+// }
+
+// var num = +prompt("number")
+// if (num > 10) {
+//     console.log("greater then 10");
+// }
+//  else { (num < 10)
+//         console.log("less then 10");
+//     }
+
+// var num = +prompt("number2")
+// if (num < 10) {
+//     console.log("less then 10")
+// }
+// else if (num > 10) {
+//     console.log("greater then 10")
+// }
+// else {
+// console.log("equal to 10")
+// }
+
+// var first = +prompt("number1");
+// var second = +prompt("number2");
+// if (first < second) {
+//     console.log("number2 big number1")
+// }
+// else if (second < first) {
+//     console.log("number1 big number2")
+// }
+// else {
+//     console.log("values are equal")
+// }
+
+// var i = 0;
+// while (i < 101) {
+//     console.log(i)
+//     i++
+// }
+
+var fiveNumber1 = +prompt("number1")
+while (fiveNumber1 < 5) {
+    document.write("hello");
+    fiveNumber1++
 }
-
-var NUM1 = 2
-var NUM2 = 2
-var NUM3 = 2
-var NUM4 = 2
-var avg = ((NUM1 + NUM2 + NUM3 + NUM4) / 4)
-var sum = (NUM1 + NUM2 + NUM3 + NUM4)
-console.log("sum", "avg")
-
-
-        // var width = 150;
-        // var Length = 100;
-        // var area = (width * Length)
-        // console.log("area")
-
-
-        // var num1 = 10;
-        // var num2 = 20;
-        // var num3 = 0;
-        // console.log(num1 + "and" + num2)
-        // num3 = num1;
-        // num1 = num2;
-        // num2 = num3;
-        // console.log(num1 + "and" + num2)
-
-        // var user = prompt("Are you a registered user?");
-        // var age = prompt("Are you 18 years old?");
-        // var hello = prompt("hello");
-
-        // var num1 = +prompt("num1");
-        // var num2 = +prompt("num2");
-        // console.log( num1,num2,)
-
-
-        //  var numb1 = +prompt("numb1");
-        //  var numb2 = +prompt("numb2");
-        //  console.log( numb1,numb2,)
-
-        // var cus = +prompt("numberr");
-        // var cus1 = +prompt("nvsc");
-        // var ttt =  prompt("dipa")
-        // console.log(cus, cus1 ,ttt)
-
-        // var a1 = "a";
-        // var a2 = "1";
-        // console.log(a1, a2)
+// var fiveNumber2 = + prompt("number2")
+// var fiveNumber3 = + prompt("number3")
+// var fiveNumber4 = + prompt("number4")
+// var fiveNumber5 = + prompt("number5")
